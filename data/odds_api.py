e06c03d86e7d189e3cc53bc2e4bedb3a9f825ae8
jrojo0153-ux <jrojo0153@gmail.com>
2026-03-30 06:58:28 -0600

    Update odds_api.py

--- data/odds_api.py
+++ data/odds_api.py
@@ -1,25 +1,45 @@
-import os
 import requests
+import os
 
-API_KEY = os.getenv("44dd2a172a7f8f0a2d6e0ab9a5534dad")
+API_KEY = os.getenv("API_KEY_ODDS")
 
 def get_odds():
-    url = "https://api.the-odds-api.com/v4/sports/soccer/odds/"
+    url = f"https://api.the-odds-api.com/v4/sports/upcoming/odds/?apiKey={API_KEY}&regions=eu&markets=h2h"
 
-    params = {
-        "apiKey": API_KEY,
-        "regions": "eu",
-        "markets": "h2h",
-        "oddsFormat": "decimal"
-    }
+    res = requests.get(url)
 
-    try:
-        res = requests.get(url, params=params)
-
-        if res.status_code != 200:
-            return []
-
-        return res.json()
-
-    except:
+    if res.status_code != 200:
+        print("❌ Error odds:", res.text)
         return []
+
+    data = res.json()
+    matches = []
+
+    for game in data:
+        try:
+            home = game["home_team"]
+            away = game["away_team"]
+            match_id = game["id"]
+
+            outcomes = game["bookmakers"][0]["markets"][0]["outcomes"]
+
+            odds = {}
+            for o in outcomes:
+                if o["name"] == home:
+                    odds["home"] = o["price"]
+                elif o["name"] == away:
+                    odds["away"] = o["price"]
+                else:
+                    odds["draw"] = o["price"]
+
+            matches.append({
+                "id": match_id,
+                "home": home,
+                "away": away,
+                "odds": odds
+            })
+
+        except:
+            continue
+
+    return matches
