8430f9e7a80cea665dcaba7d0d9fc443280828eb
jrojo0153-ux <jrojo0153@gmail.com>
2026-03-30 06:59:15 -0600

    Update storage.py

--- ml/storage.py
+++ ml/storage.py
@@ -1,28 +1,16 @@
 import json
 import os
 
-FILE_PATH = "results.json"
-
+FILE = "results.json"
 
 def save_results(data):
-    """
-    Guarda resultados históricos del bot
-    """
+    if not os.path.exists(FILE):
+        history = []
+    else:
+        with open(FILE, "r") as f:
+            history = json.load(f)
 
-    try:
-        # Si el archivo no existe → crear lista
-        if not os.path.exists(FILE_PATH):
-            history = []
-        else:
-            with open(FILE_PATH, "r") as f:
-                history = json.load(f)
+    history.append(data)
 
-        history.append(data)
-
-        with open(FILE_PATH, "w") as f:
-            json.dump(history, f, indent=2)
-
-        print("💾 Resultados guardados")
-
-    except Exception as e:
-        print("❌ Error guardando resultados:", e)
+    with open(FILE, "w") as f:
+        json.dump(history, f, indent=2)
